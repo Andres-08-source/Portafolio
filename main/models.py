1669edb269577ba2221d178c62b193744466baa6
Andrés <andresleguizamon088@gmail.com>
2025-06-04 18:14:52 -0500

    version 1 estable

--- main/models.py
+++ main/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Technology(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     link = models.URLField()
+    image = models.ImageField(upload_to="project_images/", null=True, blank=True)
     date = models.DateField(auto_now_add=False)
+    technologies = models.ManyToManyField(Technology)  # ¡Aquí la magia!
+
 
     def __str__(self):
         return self.title
+
