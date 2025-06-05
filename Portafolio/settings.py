79a7ac78080fe9efec251af78d14f3ede098ba20
Andrés <andresleguizamon088@gmail.com>
2025-06-04 19:04:43 -0500

    ajustes para lanzar app

--- Portafolio/settings.py
+++ Portafolio/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-habn_i@)m-l%526swvp_vc7yivbvp0k80#vr1*3)#@4-=0bjc4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
